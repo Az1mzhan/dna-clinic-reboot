500549b25856a90988dd2d97fd963c5fd0490850
AzimZen <azimzhaan@gmail.com>
2024-07-31 20:37:21 +0500

    Medics table and navbar refactoring

--- src/components/medics-table/MedicsTable.tsx
+++ src/components/medics-table/MedicsTable.tsx
@@ -2,7 +2,6 @@ import { defaultMedics } from "../../types/Medic";
 import {
   Avatar,
   Box,
-  FormGroup,
   Link,
   Paper,
   Table,
@@ -14,7 +13,7 @@ import {
   TextField,
 } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import styles from './MedicsTable.module.css'
+import styles from "./MedicsTable.module.css";
 
 export const MedicsTable = () => {
   return (
@@ -22,13 +21,18 @@ export const MedicsTable = () => {
       <p className={styles.title}>Наши врачи</p>
       <p className={styles.titledesc}>Наши врачи - наша гордость!</p>
       <Box className={styles.medicsForm} component={Paper}>
-        <FormGroup>
+        <div className={styles.formInput}>
           <TextField
+            className={styles.input}
             label="Имя, должность, опыт работы"
             variant="outlined"
           />
-          <TextField label="Фильтр по сведению" variant="outlined"/>
-        </FormGroup>
+          <TextField
+            className={styles.input}
+            label="Фильтр по сведению"
+            variant="outlined"
+          />
+        </div>
         <TableContainer>
           <Table>
             <TableHead>
@@ -37,20 +41,26 @@ export const MedicsTable = () => {
                 <TableCell>Должность</TableCell>
                 <TableCell>Опыт работы</TableCell>
                 <TableCell>Прием</TableCell>
-                <TableCell/>
+                <TableCell></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {defaultMedics.map((medic, id) => (
                 <TableRow className={styles.tableRow} key={id}>
                   <TableCell className={styles.medicAvatar}>
-                    <Avatar src={medic.avatar} alt={medic.fullName}/>
+                    <Avatar src={medic.avatar} alt={medic.fullName}></Avatar>
                     <p>{medic.fullName}</p>
                   </TableCell>
                   <TableCell>{medic.profession}</TableCell>
                   <TableCell>{medic.experience}</TableCell>
                   <TableCell>
-                    <button className={medic.receptionType === "free" ? styles.medicCost : styles.medicCostPaid}>
+                    <button
+                      className={
+                        medic.receptionType === "free"
+                          ? styles.medicCost
+                          : styles.medicCostPaid
+                      }
+                    >
                       {medic.receptionType === "free"
                         ? "Бесплатный"
                         : "Платный"}
