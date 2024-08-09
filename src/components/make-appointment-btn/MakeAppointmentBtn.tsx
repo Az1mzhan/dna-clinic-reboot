9b78645a6002af49ce7325c7033f52c96675b1de
AzimZen <azimzhaan@gmail.com>
2024-08-09 15:07:28 +0500

    Changelog:  * Popup implementation

--- src/components/make-appointment-btn/MakeAppointmentBtn.tsx
+++ src/components/make-appointment-btn/MakeAppointmentBtn.tsx
@@ -1,42 +1,46 @@
 import { FC, useState } from "react";
 import styles from "./make-appointment-btn.module.css";
-import { MeetingPopUp } from "../meeting-popup/MettingPopUp";
+import { MeetingPopUp } from "../meeting-popup/MeetingPopUp";
 import buttonAdornment from "../../assets/button_adornment.svg";
 
-export const MakeAppointmentBtn: FC = (props) => {
-  const [showPopUp, setShowPopUp] = useState(false)
+interface Props {
+  introductory: boolean;
+}
 
-  function show(){
-    if (showPopUp){
-      setShowPopUp(false)
-      document.body.classList.remove('popUp')
+export const MakeAppointmentBtn: FC<Props> = ({ introductory }: Props) => {
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  function show() {
+    if (showPopUp) {
+      setShowPopUp(false);
+      document.body.classList.remove("popUp");
     } else {
-      setShowPopUp(true)
-      document.body.className += 'popUp'
+      setShowPopUp(true);
+      document.body.className += "popUp";
     }
   }
-  if (props.introductory==undefined){
+  if (introductory) {
     return (
       <>
         <button className={styles.makeAppointmentBtn} onClick={show}>
           Записаться на прием
         </button>
-        {showPopUp && <MeetingPopUp setShowPopUp={setShowPopUp}/>}
+        {showPopUp && <MeetingPopUp show={show} />}
       </>
     );
   } else {
     return (
       <>
         <button className={styles.makeAppointmentBtnIntr} onClick={show}>
-            Записаться на прием
-            <img
-              src={buttonAdornment}
-              alt="buttonAdornment"
-              width={62}
-              height={62}
-            />
-          </button>
-        {showPopUp && <MeetingPopUp show={show}/>}
+          Записаться на прием
+          <img
+            src={buttonAdornment}
+            alt="buttonAdornment"
+            width={62}
+            height={62}
+          />
+        </button>
+        {showPopUp && <MeetingPopUp show={show} />}
       </>
     );
   }
---
+++ src/components/meeting-popup/MeetingPopUp.tsx
@@ -0,0 +1,18 @@
+import { FC, MouseEventHandler } from "react";
+import styles from "./MeetingPopUp.module.css";
+
+interface Props {
+  show: MouseEventHandler | undefined;
+}
+
+export const MeetingPopUp: FC<Props> = ({ show }: Props) => {
+  return (
+    <div className={styles.popUp} onClick={show}>
+      <iframe
+        id="amoforms_iframe_1388914"
+        name="amoforms_iframe_1388914"
+        src='https://forms.amocrm.ru/forms/html/form_1388914_2a52ac8f1b35dd70ef2a181d7132f5e2.html?date=1723044898#{"form_id":"1388914","form_hash":"2a52ac8f1b35dd70ef2a181d7132f5e2","user_origin":{"datetime":"Wed Aug 07 2024 20:34:58 GMT+0500 (West Kazakhstan Time)","referer":"https://ulanaitbay67.amocrm.ru/"},"is_modal":false,"success_message":"%D0%91%D0%BB%D0%B0%D0%B3%D0%BE%D0%B4%D0%B0%D1%80%D0%B8%D0%BC,%20%D1%87%D1%82%D0%BE%20%D0%B2%D1%8B%D0%B1%D1%80%D0%B0%D0%BB%D0%B8%20%D0%BD%D0%B0%D1%81!%20%D0%9C%D1%8B%20%D1%81%D0%B2%D1%8F%D0%B6%D0%B5%D0%BC%D1%81%D1%8F%20%D1%81%20%D0%B2%D0%B0%D0%BC%D0%B8%20%D0%B2%20%D1%81%D0%BA%D0%BE%D1%80%D0%BE%D0%BC%20%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8.%20%D0%92%D1%81%D1%91%20%D0%B1%D1%83%D0%B4%D0%B5%D1%82%20%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE!","has_redirect":"","is_dark_bg":true,"utm":{"source":"","medium":"","content":"","campaign":"","term":""},"ga":{},"location":"https://forms.amocrm.ru/rtmmxrd","dp":{}}'
+      ></iframe>
+    </div>
+  );
+};
